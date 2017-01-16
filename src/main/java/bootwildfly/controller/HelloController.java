dfe3c8aa16aa073d71fc41b65e8b2fcaf3296743
jiefanzhang <jiefanzhang@WSWINCNHZ1615.hengtiansoft.com>
2017-01-16 11:33:35 +0800

    add template in spirng boot .

--- src/main/java/bootwildfly/controller/HelloController.java
+++ src/main/java/bootwildfly/controller/HelloController.java
@@ -1,11 +1,11 @@
 package bootwildfly.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
